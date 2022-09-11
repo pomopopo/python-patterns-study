9f6e8c0f4b9be07e23c192ced19df1e3278b7de5
pomo <3185933991@qq.com>
2022-09-11 13:44:26 -0400

    增加对类支持比较完善、且线程安全的装饰函数

--- patterns/singleton.py
+++ patterns/singleton.py
@@ -62,14 +62,55 @@ b.my_prop         # a 的属性 b自然是有的, 前面只是 as 么
 ```
 """
 
+# """ 用函数闭包实现功能完整的单例，functools 实现对单例的封装，threading 实现同步锁，可以线程安全"""
+import functools
+import threading
+
+
+def singleton_0_func_wrap(cls):
+    """from 官方建议的实现方式，对类的支持比较完善"""
+    cls.__new_original__ = cls.__new__
+
+    @functools.wraps(cls.__new__)
+    def singleton_new(cls, *args, **kwargs):
+        # Lock 保证线程安全。CAUTION: 非必要情况启用线程锁会启动 GIL，拖慢运行速度
+        with threading.Lock():
+            it = cls.__dict__.get('__it__')
+            if it is not None:
+                return it
+            cls.__it__ = it = cls.__new_original__(cls, *args, **kwargs)
+            it.__init_original__(*args, **kwargs)
+            return it
+
+    cls.__new__ = singleton_new
+    cls.__init_original__ = cls.__init__
+    cls.__init__ = object.__init__
+    return cls
+
+
+@singleton_0_func_wrap
+class Worker_0(object):
+
+    def __new__(cls, *args, **kwargs):
+        cls.x = 10
+        return object.__new__(cls)
+
+    def __init__(self, x, y):
+        assert self.x == 10
+        self.x = x
+        self.y = y
+
+
 # """用函数闭包实现单例"""
 def singleton_1_func_wrap(cls):
+    """这个函数比较简单，不支持 worker class 的 __new__ 或者继承"""
     _instances = {}
 
     def wrapper(*args, **kwargs):
         if cls not in _instances:
             _instances[cls] = cls(*args, **kwargs)
         return _instances[cls]
+
     return wrapper
 
 
@@ -102,7 +143,7 @@ class Singleton_3_new(object):
     """new的不是真单例,每次都还会执行__init__,并且后面结果会覆盖前面的!"""
     _instance = None
 
-    def __new__(cls, *args, **kwargs) :
+    def __new__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
         return cls._instance
@@ -112,6 +153,7 @@ class Singleton_3_new(object):
         self._a = args
         self._kw = kwargs
         print(self._a, self._kw)
+
 
 # meatclass方式
 class Singleton_4_meta(type):
@@ -128,8 +170,11 @@ class Worker_4(Singleton_4_meta):
     def __init__(self, *args, **kw):
         print(args, kw)
 
+
 def main():
     """
+    # 0. 函数装饰器的线程安全单例
+
     # 1. 用 import 实现单例
     >>> import abc as a   # 用系统模块演示, 不再创建新的了
     >>> import abc as b
@@ -173,8 +218,13 @@ if __name__ == "__main__":
 
     doctest.testmod(verbose=False)
 
-    # 5. meta
-    # print('-'*20)
-    # a = Worker_4('5',name='ha')
-    # b = Worker_4()
-    # print('func', id(a) == id(b))
+
+    # 对线程安全的函数装饰器单例测试，看打印结果
+    def worker0():
+        s = Worker_0(3, 7)
+        print(id(s))
+
+
+    tasks = [threading.Thread(target=worker0) for _ in range(5)]
+    [t.start() for t in tasks]
+    [t.join() for t in tasks]
